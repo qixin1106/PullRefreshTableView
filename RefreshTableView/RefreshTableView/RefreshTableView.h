f78ddd73e8c4b91d94ece16d70ae686416e29ae7
Qixin <qixin1106@qq.com>
2014-08-21 15:54:37 +0800

    增加使用帮助

--- RefreshTableView/RefreshTableView/RefreshTableView.h
+++ RefreshTableView/RefreshTableView/RefreshTableView.h
@@ -6,6 +6,19 @@
 //  Copyright (c) 2014年 Qixin. All rights reserved.
 //
 
+/*
+ 使用方式
+
+ self.tableView = [[RefreshTableView alloc] initWithFrame:CGRectMake(0, 64, 320, screenHeight-64)
+                                                    style:UITableViewStylePlain
+                                          isPullDownValid:YES
+                                            isPullUpValid:YES];
+ self.tableView.delegate = self;
+ self.tableView.dataSource = self;
+ [self.view addSubview:self.tableView];
+
+ */
+
 #import <UIKit/UIKit.h>
 
 @class PullRefreshView;
